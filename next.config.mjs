a1ceca2b3faffbb2df6e175451a15aa096ae798a
Amanayara Slot <amanayaradev@gmail.com>
2024-05-23 21:23:29 -0300

    feat: finalizando projeto

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-// /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['mks-sistemas.nyc3.digitaloceanspaces.com'],
-  }
-};
-
-export default nextConfig;
--- src/components/footer/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./style.module.scss";
-
-export function Footer() {
-  return(
-    <footer className={styles.footer}>
-      <h2>MKS sistemas © Todos os direitos reservados</h2>
-    </footer>
-  )
-}
