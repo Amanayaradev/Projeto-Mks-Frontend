45649453a904b8bbd78d7d4ad8bd698dea1c2dc0
Amanayara Slot <amanayaradev@gmail.com>
2024-05-23 21:06:53 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import Cards from "../components/cards/page";
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+export default function Home() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <main>
+        <Cards/>
+      </main>
+    </QueryClientProvider>
+  );
+}
---
+++ src/components/carrinho/page.tsx
@@ -0,0 +1,104 @@
+import styles from "./style.module.scss";
+import { PropsProduto } from "../cards/page";
+import Image from "next/image";
+import excluir from "../../../public/fechar.svg";
+import { useEffect, useState } from "react";
+
+const handleRemoveItem = (product: PropsProduto, setProducts: React.Dispatch<React.SetStateAction<PropsProduto[]>>) => {
+  console.log("params", product);
+
+  const storedProducts = localStorage.getItem('products');
+  const products = storedProducts ? JSON.parse(storedProducts) : [];
+  const updatedProducts: PropsProduto[] = products.filter((prod: PropsProduto) => prod.id !== product.id);
+
+  console.log("return", products);
+  console.log(updatedProducts);
+  localStorage.setItem('products', JSON.stringify(updatedProducts));
+  setProducts(updatedProducts);
+}
+
+const handleDimunue = (product: PropsProduto, setProducts: React.Dispatch<React.SetStateAction<PropsProduto[]>>) => {
+  const storedProducts = localStorage.getItem('products');
+  const products = storedProducts ? JSON.parse(storedProducts) : [];
+
+  const index = products.findIndex((prod: PropsProduto) => prod.id === product.id);
+
+  if (index !== -1) {
+    const updatedProducts = [...products];
+    updatedProducts.splice(index, 1);
+
+    localStorage.setItem('products', JSON.stringify(updatedProducts));
+    setProducts(updatedProducts);
+  }
+}
+
+const handleAddItem = (product: PropsProduto, setProducts: React.Dispatch<React.SetStateAction<PropsProduto[]>>) => {
+  const storedProducts = localStorage.getItem('products');
+  const products = storedProducts ? JSON.parse(storedProducts) : [];
+
+  const updatedProducts: PropsProduto[] = [...products, product];
+
+  localStorage.setItem('products', JSON.stringify(updatedProducts));
+  setProducts(updatedProducts);
+}
+
+
+export default function Carrinho() {
+  const [products, setProducts] = useState<PropsProduto[]>([]);
+  const [values, setValues] = useState<string[]>(["0"]);
+  console.log("values", values)
+    useEffect(() => {
+      const storedIdsString: string | null = localStorage.getItem('products');
+      const storedBuy: PropsProduto[] = storedIdsString ? JSON.parse(storedIdsString) : [];
+      const val = storedBuy.map((val) => (val.price))
+      setValues(val)
+      setProducts(storedBuy);
+    }, []);
+  const sum = values.reduce((total, num) => total + parseFloat(num), 0);
+  return (
+    <section className={styles.carrinho}>
+      <div className={styles.paddingCar}>
+        <span className={styles.carrinhobox1}>
+          <span className={styles.carrinhoH2}>
+            <h2>Carrinho <br/> de compras</h2>
+          </span>
+        </span>
+        <div className={styles.ulContainer}>
+          <ul className={styles.ul}>
+            {products?.map((buy) => (
+              <li key={buy.id}>
+
+                <div className={styles.boxCar}>
+                  <div className={styles.boxImg}>
+                    <Image className={styles.image} src={buy.photo} alt={buy.name} width={60} height={60} />
+                    <p>{buy.name}</p>
+                  </div>
+                  <div className={styles.boxSpan}>
+                    <p className={styles.quantityLabel}>qtd:</p>
+                    <div className={styles.buttons}>
+                    <button type="submit" onClick={() => handleDimunue(buy, setProducts)}>-</button>
+                    <button>{/* Aqui você pode colocar a quantidade do item */}</button>
+                    <button onClick={() => handleAddItem(buy, setProducts)}>+</button>
+                  </div>
+
+                  </div>
+                  <span>R${buy.price}</span>
+                  <div className={styles.boxExcluir}>
+                    <button className={styles.btnExcluir} onClick={() => handleRemoveItem(buy, setProducts)}>
+                      <Image src={excluir} alt="fechar carrinho de compras" width={12} height={12} />
+                    </button> 
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.total}>
+          <h3>Total:</h3>
+          <span>R${sum.toFixed(2)}</span>
+        </div>
+      </div>
+      <button className={styles.button} type="submit">Finalizar Compra</button>
+    </section>
+  );
+}
